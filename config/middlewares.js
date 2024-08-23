19d5cd73ae8d2045bc188d9e3b5cc43499b3c487
israrahmedjan <israrahmedjan@gmail.com>
2024-08-23 19:47:32 +0500

    Images uploaded on local folder like upload

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,32 +1,33 @@
 module.exports = [
   'strapi::logger',
   'strapi::errors',
-  {
-    name: 'strapi::security',
-    config: {
-      contentSecurityPolicy: {
-        useDefaults: true,
-        directives: {
-          'connect-src': ["'self'", 'https:'],
-          'img-src': [
-            "'self'", 
-            'data:', 
-            'blob:', 
-            'res.cloudinary.com', 
-            'https://best-trust-c63ee18b56.media.strapiapp.com' // Allow images from this domain
-          ],
-          'media-src': [
-            "'self'",
-            'data:',
-            'blob:',
-            'res.cloudinary.com',
-            'https://best-trust-c63ee18b56.media.strapiapp.com' // Allow media from this domain
-          ],
-          upgradeInsecureRequests: null,
-        },
-      },
-    },
-  },
+  'strapi::security',
+  // {
+  //   name: 'strapi::security',
+  //   config: {
+  //     contentSecurityPolicy: {
+  //       useDefaults: true,
+  //       directives: {
+  //         'connect-src': ["'self'", 'https:'],
+  //         'img-src': [
+  //           "'self'", 
+  //           'data:', 
+  //           'blob:', 
+  //           'res.cloudinary.com', 
+  //           'https://best-trust-c63ee18b56.media.strapiapp.com' // Allow images from this domain
+  //         ],
+  //         'media-src': [
+  //           "'self'",
+  //           'data:',
+  //           'blob:',
+  //           'res.cloudinary.com',
+  //           'https://best-trust-c63ee18b56.media.strapiapp.com' // Allow media from this domain
+  //         ],
+  //         upgradeInsecureRequests: null,
+  //       },
+  //     },
+  //   },
+  // },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
