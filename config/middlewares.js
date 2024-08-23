f308034110e7bff08fb780210b7b6601c56e7b2e
israrahmedjan <israrahmedjan@gmail.com>
2024-08-23 18:33:04 +0500

    New code Added

--- config/middlewares.js
+++ config/middlewares.js
@@ -7,24 +7,20 @@ module.exports = [
       contentSecurityPolicy: {
         useDefaults: true,
         directives: {
-          'default-src': ["'self'"],
-          'connect-src': ["'self'", 'https:', 'https://api.cloudinary.com'], // Allow connections to Cloudinary API
+          'connect-src': ["'self'", 'https:'],
           'img-src': [
-            "'self'",
-            'data:',
-            'blob:',
-            'market-assets.strapi.io',
-            'res.cloudinary.com', // Allow images from Cloudinary
-            'https://best-trust-c63ee18b56.media.strapiapp.com', // Allow images from Strapi media domain
-            '*.strapi.io'
+            "'self'", 
+            'data:', 
+            'blob:', 
+            'market-assets.strapi.io', 
+            'res.cloudinary.com'
           ],
           'media-src': [
             "'self'",
             'data:',
             'blob:',
             'market-assets.strapi.io',
-            'res.cloudinary.com', // Allow media from Cloudinary
-            'https://best-trust-c63ee18b56.media.strapiapp.com' // Allow media from Strapi media domain
+            'res.cloudinary.com',
           ],
           upgradeInsecureRequests: null,
         },
